ddaddd5ac59a87f9a676b05ffa31332105bdd544
Wonjun Jeong <nttejun@gmail.com>
2017-12-27 08:53:58 +0900

    Add stream and parallelStream

--- JavaInAction/src/Basic01/filter/CellphoneMain.java
+++ JavaInAction/src/Basic01/filter/CellphoneMain.java
@@ -2,6 +2,7 @@ package Basic01.filter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CellphoneMain {
     public static void main(String[] args) {
@@ -14,7 +15,14 @@ public class CellphoneMain {
 
         // 람다
         cellphone.filterCellphone(storage, (Cellphone c) -> "IPhone".equals(c.getCompany()));
-        cellphone.filterCellphone(storage, (Cellphone c) -> "IPhone".equals(c.getModel()));
+        cellphone.filterCellphone(storage, (Cellphone c) -> "IPhoneX".equals(c.getModel()));
+
+        // 순차처리 방식
+        List<Cellphone> iPhoneCellphone = storage.stream().filter((Cellphone c) -> c.getCompany().equals("IPhone")).collect(Collectors.toList());
+
+        // 병렬처리 방식
+        List<Cellphone> iPhoneXCellphone = storage.parallelStream().filter((Cellphone c) -> c.getModel().equals("IPhoneX")).collect(Collectors.toList());
+
 
     }
 }
