74219e965f756902a0cd086c2c1dc19d395053b8
Wonjun Jeong <nttejun@gmail.com>
2018-03-03 17:42:27 +0900

    An abstract class that determines which interface (APIs) an instance needs

--- JavaDesignPatternBasic/factoryMethodProduct/Factory.java
+++ JavaDesignPatternBasic/factoryMethodProduct/Factory.java
@@ -1,5 +1,7 @@
 package factoryMethodProduct;
 
+// 상위클래스로 골격 제공
+//인스턴스가 필요한 인터페이스(API)를 결정하는 추상클래스
 public abstract class Factory {
 	public final Product create(String owner) {
 		Product product = createProduct(owner);
--- JavaDesignPatternBasic/factoryMethodProduct/Product.java
+++ JavaDesignPatternBasic/factoryMethodProduct/Product.java
@@ -1,5 +1,8 @@
 package factoryMethodProduct;
 
+// 상위클래로 골격 제공
+// 인스턴스가 필요한 인터페이스(API)를 결정하는 추상클래스
+
 public abstract class Product {
 	public abstract void use();
 }
