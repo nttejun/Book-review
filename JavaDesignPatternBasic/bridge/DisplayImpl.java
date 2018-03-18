c13a5b79c22f1d15641584c131d2d61206e4670c
Wonjun Jeong <nttejun@gmail.com>
2018-03-18 10:12:10 +0900

    Display 추상화, Display 기능 구현의 최상위 클래스

---
+++ JavaDesignPatternBasic/bridge/DisplayImpl.java
@@ -0,0 +1,7 @@
+package bridge;
+// 기능을 구현하는 추상클래스
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+    public abstract void rawPrint();
+    public abstract void rawClose();
+}
