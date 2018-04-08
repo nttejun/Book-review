1fa58c2bbd02621c0e13f1224a019dd3f1d688c0
Wonjun Jeong <nttejun@gmail.com>
2018-04-08 17:45:36 +0900

    환영 페이지 틀 내용을 만드는 역할

---
+++ JavaDesignPatternBasic/facade/HtmlWriter.java
@@ -0,0 +1,36 @@
+package facade;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class HtmlWriter{
+    private Writer writer;
+    public HtmlWriter (Writer writer){
+        this.writer = writer;
+    }
+
+    public void title(String title) throws IOException {
+        writer.write("<html>");
+        writer.write("<head>");
+        writer.write("<title>" + title + "</title>");
+        writer.write("</head>");
+        writer.write("<body> " + title + " 본문");
+    }
+
+    public void paragraph(String msg) throws IOException{
+        writer.write("<p>" + msg + "</p>");
+    }
+
+    public void link(String href, String caption) throws IOException{
+        paragraph("<a href=\""+href+"\">" + caption + "</a>");
+    }
+
+    public void mailto(String mailaddr, String username) throws IOException{
+        link("mailto:"+mailaddr, username);
+    }
+    public void close() throws IOException {
+        writer.write("</body>");
+        writer.write("</html>\n");
+        writer.close();
+    }
+}
